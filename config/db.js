f0074b788ae2aafee0af388a5d6cd07fb65f3490
jshin02 <m.jinhyun.shin@gmail.com>
2020-07-06 12:31:57 -0400

    Updated config url

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'express-api-template'
+const mongooseBaseName = 'damp-shore-57998'
 
 // create the mongodb uri for development and test
 const database = {
